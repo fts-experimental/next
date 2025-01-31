ae9802c0c4e13df0643b88ac67be6afe5144ff1e
ftsmasaki <masaki@fts-com.jp>
2025-01-31 13:59:32 +0900

    feat: [/hello/:postId] APIエンドポイントを作成

--- src/features/hello/api/route.spec.ts
+++ src/features/hello/api/route.spec.ts
@@ -9,6 +9,7 @@ describe("/hello", () => {
       const url = client.api.hello.$url();
 
       const res = await fetcher<ResType>(url);
+      expect(res.isOk()).toBe(true);
       if (res.isOk()) {
         expect(res.value.message).toBe("hello");
       }
@@ -18,13 +19,30 @@ describe("/hello", () => {
       type ResType = InferResponseType<
         (typeof client.api.hello)[":postId"]["$get"]
       >;
-      const url = client.api.hello.$url({
-        param: { postId: "1" },
+      const validParam = { postId: "1" };
+      const url = client.api.hello[":postId"].$url({
+        param: validParam,
       });
 
       const res = await fetcher<ResType>(url);
+      expect(res.isOk()).toBe(true);
       if (res.isOk()) {
         expect(res.value.message).toBe("hello 1");
+      }
+    });
+
+    it("不正なパスパラメータを渡した場合はエラーを返す", async () => {
+      type ResType = InferResponseType<
+        (typeof client.api.hello)[":postId"]["$get"]
+      >;
+      const invalidParam = { postId: "hoge" };
+      const url = client.api.hello[":postId"].$url({
+        param: invalidParam,
+      });
+
+      const res = await fetcher<ResType>(url);
+      if (res.isErr()) {
+        expect(res.error.status).toBe(400);
       }
     });
   });
--- src/features/hello/api/route.ts
+++ src/features/hello/api/route.ts
@@ -1,12 +1,40 @@
 import { Hono } from "hono";
+import { validator } from "hono/validator";
+import { z } from "zod";
 
 const app = new Hono()
   .get("/", async (c) => {
     return c.json({ message: "hello" });
   })
-  .get("/:postId", async (c) => {
-    const { postId } = c.req.param();
-    return c.json({ message: `hello ${postId}` });
-  });
+  .get(
+    "/:postId",
+    validator("param", (value, c) => {
+      const schema = z.object({
+        postId: z.coerce.number().min(1, "PostID is required"),
+      });
+
+      const result = schema.safeParse(value);
+
+      if (!result.success) {
+        return c.json(
+          {
+            success: false,
+            message: "Validation failed",
+            errors: result.error.errors,
+          },
+          400
+        );
+      }
+
+      return result.data;
+    }),
+    async (c) => {
+      const { postId } = c.req.valid("param");
+      return c.json({
+        success: true,
+        message: `hello ${postId}`,
+      });
+    }
+  );
 
 export default app;
