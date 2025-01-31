b432891fbef4b2b4e6ab5bfb5a4c7d0d2b7fe076
ftsmasaki <masaki@fts-com.jp>
2025-01-31 17:26:44 +0900

    feat: [features/hello/components/message]  カスタムフックを使用してデータ取得するコンポーネントの作成例

---
+++ src/features/hello/components/message.tsx
@@ -0,0 +1,9 @@
+import { useHello } from "@/features/hello/hooks/use-hello";
+
+export const Message = () => {
+  const { data } = useHello();
+
+  if (!data?.isOk()) return null;
+
+  return data.value.message;
+};
