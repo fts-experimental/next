c177442ba07e3aeca202c427567c037fcb2922a1
ftsmasaki <masaki@fts-com.jp>
2025-01-31 13:56:39 +0900

    refact: 環境変数の名称を変更

--- src/libs/rpc.ts
+++ src/libs/rpc.ts
@@ -1,10 +1,10 @@
 import { hc } from "hono/client";
-
-import type { AppType } from "@/app/api/[[...route]]/route";
 import { env } from "@/config/env";
 
-if (!env.NEXT_PUBLIC_API_URL) {
-  throw new Error("NEXT_PUBLIC_API_URL is not set");
+import type { AppType } from "@/app/api/[[...route]]/route";
+
+if (!env.NEXT_PUBLIC_APP_URL) {
+  throw new Error("NEXT_PUBLIC_APP_URL is not set");
 }
 
-export const client = hc<AppType>(env.NEXT_PUBLIC_API_URL);
+export const client = hc<AppType>(env.NEXT_PUBLIC_APP_URL);
