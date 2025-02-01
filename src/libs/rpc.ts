becf6a2ee166ce93cb4f573275c7d55776d359d5
ftsmasaki <masaki@fts-com.jp>
2025-02-01 20:07:03 +0900

    feat: rpc.tsの例外スロー部分に到達することはないので当該処理を削除

--- src/libs/rpc.ts
+++ src/libs/rpc.ts
@@ -3,8 +3,4 @@ import { env } from "@/config/env";
 
 import type { AppType } from "@/app/api/[[...route]]/route";
 
-if (!env.NEXT_PUBLIC_APP_URL) {
-  throw new Error("NEXT_PUBLIC_APP_URL is not set");
-}
-
 export const client = hc<AppType>(env.NEXT_PUBLIC_APP_URL);
