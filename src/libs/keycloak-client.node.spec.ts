dfaf3ddef40fcafdfb107b06ffc127f333e552a1
ftsmasaki <masaki@fts-com.jp>
2025-02-06 15:38:36 +0900

    refact: getUsers関数のみをエクスポート

--- src/libs/keycloak-client.node.spec.ts
+++ src/libs/keycloak-client.node.spec.ts
@@ -1,24 +1,8 @@
-import { env } from "@/config/env";
-import { getAccessToken } from "@/libs/get-access-token";
-import { client } from "./openapi";
-import { paths } from "@/types/openapi/keycloak";
+import { getUsers } from "@/libs/keycloak-client";
 
 describe("keycloak-client", () => {
   it("ユーザーを取得する", async () => {
-    const accessToken = await getAccessToken();
-
-    const { data, error } = await client<paths>({
-      baseUrl: env.KEYCLOAK_BASE_URL,
-    }).GET("/admin/realms/{realm}/users", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      params: {
-        path: {
-          realm: env.KEYCLOAK_REALM,
-        },
-      },
-    });
+    const { data, error } = await getUsers();
 
     expect(data).toBeDefined();
     expect(error).toBeUndefined();
--- src/libs/keycloak-client.ts
+++ src/libs/keycloak-client.ts
@@ -1,23 +1,23 @@
 import { env } from "@/config/env";
 import { getAccessToken } from "@/libs/get-access-token";
-import { client } from "./openapi";
+import { client } from "@/libs/openapi";
 import { paths } from "@/types/openapi/keycloak";
 
-export const keycloakClient = {
-  getUsers: async () => {
-    const accessToken = await getAccessToken();
+const getUsers = async () => {
+  const accessToken = await getAccessToken();
 
-    return client<paths>({
-      baseUrl: env.KEYCLOAK_BASE_URL,
-    }).GET("/admin/realms/{realm}/users", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
+  return client<paths>({
+    baseUrl: env.KEYCLOAK_BASE_URL,
+  }).GET("/admin/realms/{realm}/users", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    params: {
+      path: {
+        realm: env.KEYCLOAK_REALM,
       },
-      params: {
-        path: {
-          realm: env.KEYCLOAK_REALM,
-        },
-      },
-    });
-  },
+    },
+  });
 };
+
+export { getUsers };
