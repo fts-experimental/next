f71e3fa80d515d11ee7ca7ec3f7f7466c2475b2b
ftsmasaki <masaki@fts-com.jp>
2025-02-06 18:04:16 +0900

    feat: [createUser] Keycloakユーザーを作成するユーティリティ関数を作成

--- src/libs/keycloak-client.node.spec.ts
+++ src/libs/keycloak-client.node.spec.ts
@@ -1,4 +1,6 @@
-import { getUsers } from "@/libs/keycloak-client";
+import { getUsers, createUser } from "@/libs/keycloak-client";
+import { todo } from "node:test";
+import { v4 as uuid } from "uuid";
 
 describe("keycloak-client", () => {
   it("ユーザーを取得する", async () => {
@@ -7,4 +9,17 @@ describe("keycloak-client", () => {
     expect(data).toBeDefined();
     expect(error).toBeUndefined();
   });
+
+  it("ユーザーを作成する", async () => {
+    const result = await createUser({
+      email: `${uuid()}@example.com`,
+      enabled: true,
+    });
+
+    expect(result.isOk()).toBe(true);
+    expect(result._unsafeUnwrap()).toBeDefined();
+    expect(result._unsafeUnwrap().email).toBe("user1@example.com");
+  });
+
+  todo("単一のユーザーを取得する", () => {});
 });
--- src/libs/keycloak-client.ts
+++ src/libs/keycloak-client.ts
@@ -1,7 +1,8 @@
 import { env } from "@/config/env";
 import { getAccessToken } from "@/libs/get-access-token";
 import { client } from "@/libs/openapi";
-import { paths } from "@/types/openapi/keycloak";
+import { components, paths } from "@/types/openapi/keycloak";
+import { hundleResponse } from "@/libs/result";
 
 const getUsers = async () => {
   const accessToken = await getAccessToken();
@@ -20,4 +21,31 @@ const getUsers = async () => {
   });
 };
 
-export { getUsers };
+type UserRepresentation = components["schemas"]["UserRepresentation"];
+
+const createUser = async (user: UserRepresentation) => {
+  const accessToken = await getAccessToken();
+
+  const { response } = await client<paths>({
+    baseUrl: env.KEYCLOAK_BASE_URL,
+  }).POST("/admin/realms/{realm}/users", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    params: {
+      path: {
+        realm: env.KEYCLOAK_REALM,
+      },
+    },
+    body: user,
+  });
+
+  // TODO: getUser関数を作成し、作成したユーザーを返却するようにする
+  const createdUser: UserRepresentation = {
+    email: "user1@example.com",
+  };
+
+  return await hundleResponse<UserRepresentation>(response, createdUser);
+};
+
+export { getUsers, createUser };
